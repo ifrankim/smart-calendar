ac3825d99f899beb52d225616fc85cfaa7ed0741
ifrankim <fkgaieski@gmail.com>
2024-11-25 19:35:53 +0800

    fix(back/run): added port for production

--- backend/run.py
+++ backend/run.py
@@ -1,3 +1,4 @@
+import os
 from app import create_app
 from app.routes import routes
 
@@ -5,4 +6,5 @@ app = create_app()
 app.register_blueprint(routes)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
